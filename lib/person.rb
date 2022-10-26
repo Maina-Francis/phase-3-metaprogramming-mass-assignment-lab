561c292da9eb458379f0547b4e1492a5b9d9bffa
Francis Maina <sebastianfrancix@gmail.com>
2022-10-26 12:54:37 +0300

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
+# require 'pry'
 class Person
-  # your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+
+# susan_attributes = { name: "Susan", height: "5'11\"", eye_color: "Green" }
+
+# bob_attributes = { name: "Bob", hair_color: "Brown" }
+
+# binding.pry
